363a970e0125694b100f9b8331d78de54f19a345
thabitfuad <thabitfuad@nyu.edu>
2025-03-03 15:22:33 +0400

    made changes
    pylint pylint_demo.py

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,10 +1,15 @@
-def sum(a, b):
+"""A simple demo of PyLint."""
+
+def sum_custom(a, b):
+    """Adds two integers."""
     return a + b
 
 
 def hello(name):
+    """Greets the person with the provided
+    name."""
     print(f"Hello, {name}!")
 
 
 hello("Alice")
-hello()
+# hello()
